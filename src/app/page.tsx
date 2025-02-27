fdb9055d336cca9bf1aa0dbc047d74e729aaa641
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-02-27 18:09:38 -0300

    feat: add mobile support to Sidebar component with responsive design and Sheet menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
-     <main>
-      <h1 className='text-2x1'>Projeto Dashboard</h1>
-     </main>
-  )
+    <main>
+      <h1 className="text-2x1">Projeto Dashboard</h1>
+     
+    </main>
+  );
 }
