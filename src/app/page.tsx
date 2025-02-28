f0cdeeba64665f4a507b27dd013a5dc4e7aefc40
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-02-28 10:25:42 -0300

    feat: add new Cards to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,90 @@
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { CreditCard, DollarSign, Percent, Users } from "lucide-react";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2x1">Projeto Dashboard</h1>
-     
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center ">
+              <CardTitle className="text-lg sm:text-x1 text-gray-800">
+                Total Vendas
+                </CardTitle>
+                <DollarSign className="ml-auto w-4 h-4"/>
+            </div>
+            <CardDescription>
+              Vendas realizadas em 90 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center ">
+              <CardTitle className="text-lg sm:text-x1 text-gray-800">
+                Novos Clientes
+                </CardTitle>
+                <Users className="ml-auto w-4 h-4"/>
+            </div>
+            <CardDescription>
+              Últimos clientes em 30 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">1294</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center ">
+              <CardTitle className="text-lg sm:text-x1 text-gray-800 select-none">
+                Pedidos hoje
+                </CardTitle>
+                <Percent className="ml-auto w-4 h-4"/>
+            </div>
+            <CardDescription>
+              Total de pedidos hoje
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">113</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center ">
+              <CardTitle className="text-lg sm:text-x1 text-gray-800 select-none">
+                Pagamentos pendentes
+                </CardTitle>
+                <CreditCard className="ml-auto w-4 h-4"/>
+            </div>
+            <CardDescription>
+              Total de pagamentos pendentes em 30 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">83</p>
+          </CardContent>
+        </Card>
+      </section>
+
+      <section className="mt-4 flex flex-col md:flex-row gap-4"></section>
     </main>
   );
 }
